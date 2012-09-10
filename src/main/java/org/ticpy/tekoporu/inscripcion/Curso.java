76caca58936fa39e484bbf54e9180ee05ab59594
Jorge R. Castillo C <jcastillocareaga@gmail.com>
2012-09-10 09:44:46 -0400

    Inscripcion Cap. 2 Lección 1 (Logger)

--- src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
@@ -3,12 +3,21 @@ package org.ticpy.tekoporu.inscripcion;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
 public class Curso {
 	
 	private List<String> alumnosMatriculados = new ArrayList<String>();
 
+	@Inject
+	private Logger logger;
+	
 	public void matricular(String alumno) {
 		alumnosMatriculados.add(alumno);
+		logger.info("Aluno matriculado con éxito");
+
 	}
 
 	public boolean estaMatriculado(String alumno) {
