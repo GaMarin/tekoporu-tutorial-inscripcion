7f87d328376fe2d998eab1be966944c603bc2357
Jorge R. Castillo C <jcastillocareaga@gmail.com>
2012-09-18 14:58:31 -0400

    Inscripcion Cap. 5 Lección 5 (Inicializadores y Finalizadores)

--- src/main/java/org/ticpy/tekoporu/inscripcion/business/CursoBC.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/business/CursoBC.java
@@ -5,11 +5,11 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
+import org.ticpy.tekoporu.annotation.Startup;
 import org.ticpy.tekoporu.exception.ExceptionHandler;
 import org.ticpy.tekoporu.inscripcion.config.InscripcionConfig;
 import org.ticpy.tekoporu.inscripcion.domain.Alumno;
 import org.ticpy.tekoporu.inscripcion.exception.CursoException;
-import org.ticpy.tekoporu.inscripcion.persistence.AlumnoDAO;
 import org.ticpy.tekoporu.stereotype.BusinessController;
 import org.ticpy.tekoporu.transaction.Transactional;
 import org.ticpy.tekoporu.util.ResourceBundle;
@@ -18,7 +18,7 @@ import org.ticpy.tekoporu.util.ResourceBundle;
 @BusinessController
 public class CursoBC {
 
-	@Inject 
+	@Inject
 	private AlumnoBC alumnoBC;
 
 	@Inject
@@ -37,7 +37,7 @@ public class CursoBC {
 						.getCapacidadCurso()) {
 			throw new CursoException();
 		}
-		
+
 		alumnoBC.insert(alumno);
 		logger.info(bundle.getString("matricula.exito", alumno));
 
@@ -55,7 +55,13 @@ public class CursoBC {
 	}
 
 	private List<Alumno> obtenerAlumnosMatriculados() {
-		
+
 		return alumnoBC.findAll();
 	}
+
+	@Startup
+	public void iniciar() {
+		logger.info("Iniciando el CursoBC...");
+	}
+
 }
