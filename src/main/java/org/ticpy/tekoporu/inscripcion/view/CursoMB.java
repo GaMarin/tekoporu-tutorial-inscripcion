115ee438187b20449d695eb5be7ac497b14135c5
Luis Ojeda <lojeda@setics.gov.py>
2012-09-27 15:00:05 -0400

    Inscripcion Cap. 7 Leccion 4 (Crear server en Eclipse)

--- src/main/java/org/ticpy/tekoporu/inscripcion/view/CursoMB.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/view/CursoMB.java
@@ -13,9 +13,25 @@ public class CursoMB {
 
 	@Inject 
 	private CursoBC bc;
-	
+
+	private String nombreAlumno;
+ 
 	public List<Alumno> getAlumnosMatriculados(){
 		return bc.obtenerAlumnosMatriculados();
+	}
+
+	public void setNombreAlumno(String nombreAlumno) {
+
+		this.nombreAlumno = nombreAlumno;
+	}
+
+	public String getNombreAlumno() {
+
+		return nombreAlumno;
+	}
+	
+	public void matricular() {
+		bc.matricular(new Alumno(this.getNombreAlumno()));
 	}
 
 }
