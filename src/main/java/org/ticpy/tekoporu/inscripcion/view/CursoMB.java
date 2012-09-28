d8e92666deb64f19fb28b9aa88e417df4f1b3ed1
Luis Ojeda <lojeda@setics.gov.py>
2012-09-28 09:56:58 -0400

    Inscripcion Cap. 8 Leccion 1(FacesMessage)

--- src/main/java/org/ticpy/tekoporu/inscripcion/view/CursoMB.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/view/CursoMB.java
@@ -1,9 +1,9 @@
 package org.ticpy.tekoporu.inscripcion.view;
 
 import java.util.List;
-
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-
 import org.ticpy.tekoporu.inscripcion.business.CursoBC;
 import org.ticpy.tekoporu.inscripcion.domain.Alumno;
 import org.ticpy.tekoporu.stereotype.ViewController;
@@ -11,12 +11,16 @@ import org.ticpy.tekoporu.stereotype.ViewController;
 @ViewController
 public class CursoMB {
 
-	@Inject 
+	@Inject
 	private CursoBC bc;
 
+	@Inject
+	private FacesContext facesContext;
+
 	private String nombreAlumno;
- 
-	public List<Alumno> getAlumnosMatriculados(){
+
+	public List<Alumno> getAlumnosMatriculados() {
+
 		return bc.obtenerAlumnosMatriculados();
 	}
 
@@ -29,9 +33,12 @@ public class CursoMB {
 
 		return nombreAlumno;
 	}
-	
+
 	public void matricular() {
 		bc.matricular(new Alumno(this.getNombreAlumno()));
-	}
+		facesContext.addMessage("sucesso", new FacesMessage("Registro realizado con éxito"));
 
+
+
+	}
 }
