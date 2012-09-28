80c4f0795f493fe9dc17ba3ca3c47331f8c9d8e9
Luis Ojeda <lojeda@setics.gov.py>
2012-09-28 10:59:12 -0400

    Inscripcion Cap.8 Leccion 3 ( Excepciones)

--- src/main/java/org/ticpy/tekoporu/inscripcion/exception/CursoException.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/exception/CursoException.java
@@ -2,7 +2,9 @@ package org.ticpy.tekoporu.inscripcion.exception;
 
 import org.ticpy.tekoporu.exception.ApplicationException;
 
-@ApplicationException(rollback=true)
-public class CursoException extends RuntimeException{
-
+@ApplicationException(rollback = true)
+public class CursoException extends RuntimeException {
+	public CursoException() {
+		super("Error al matricular!");
+	}
 }
